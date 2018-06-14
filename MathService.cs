cd7a5250402e6ecee9efc7ebd9ea1ad40a250c79
Steve Bishop <xipooo@gmail.com>
2018-06-13 19:29:33 -0700

    Inverted dependency

--- MathService.cs
+++ MathService.cs
@@ -3,17 +3,24 @@ namespace CSharp_Delegates
     public class MathService
     {
         public delegate double ResultHandler(double value1, double value2);
+        public delegate void OutboundHandler(double result);
 
         public ResultHandler MathDelegate;
+        public OutboundHandler OutboundDelegate;
         public MathService(){
             MathDelegate = AddNumbers;
             MathDelegate += MultiplyNumbers;
         }
-        private double AddNumbers(double value1, double value2){
-            return value1 + value2;
+
+        public double AddNumbers(double value1, double value2){
+            var result = value1 + value2;
+            OutboundDelegate(result);
+            return result;
         }
-        private double MultiplyNumbers(double value1, double value2){
-            return value1 * value2;
+        public double MultiplyNumbers(double value1, double value2){
+            var result = value1 * value2;
+            OutboundDelegate(result);
+            return result;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,8 +7,11 @@ namespace CSharp_Delegates
         static void Main(string[] args)
         {
             MathService service = new MathService();
+            service.OutboundDelegate += OnOutboundDelegate;
+
             var result = service.MathDelegate(4.5, 5.78);
-            
+        }
+        static void OnOutboundDelegate(double result){
             Console.WriteLine("Result: " + result);
         }
     }
