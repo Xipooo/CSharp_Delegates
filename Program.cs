8515f640565c99b40f51d39cc0d3d7f74ad663fa
Steve Bishop <xipooo@gmail.com>
2018-06-13 18:44:19 -0700

    Intial Project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp_Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
